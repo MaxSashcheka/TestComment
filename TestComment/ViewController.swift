5a559b395dae22509d45c9c059d004a5daff0f0f
Maksim Sashcheka <maksim.sashcheka@yellow.systems>
2023-09-07 13:36:11 +0300

    Initial Commit

---
+++ TestComment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestComment
+//
+//  Created by Maksim Sashcheka on 7.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
